215d093c6cb54ede53bd1eda8b5f8a88e41a9d65
Ryan Li <ryan.li@yale.edu>
2022-06-08 12:43:17 -0400

    day 8

---
+++ June Problems/8-ab_palindrome.cpp
@@ -0,0 +1,19 @@
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalin(const string &s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            if (s[i++] != s[j--]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    int removePalindromeSub(string s) {
+        return (isPalin(s)) ? 1 : 2;
+    }
+};
